2af5eaf728653dfafffddc6abeb62dbf8637feb9
Lucas Araujo <lcsaraujo88@gmail.com>
2023-07-16 14:20:59 -0300

    GET game by id

--- src/main/java/com/devsuperior/dlist/services/GameService.java
+++ src/main/java/com/devsuperior/dlist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dlist.dto.GameDTO;
 import com.devsuperior.dlist.dto.GameMinDTO;
 import com.devsuperior.dlist.entities.Game;
 import com.devsuperior.dlist.repositories.GameRepository;
@@ -13,6 +15,17 @@ import com.devsuperior.dlist.repositories.GameRepository;
 public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly= true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		//converter Game p/ GameDTO
+		GameDTO dto = new GameDTO(result);
+		return dto;
+		
+	}
+	
+	@Transactional(readOnly= true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll(); // vai receber Game porem tem q entregar um DTO
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // trás o dado reduzido 
