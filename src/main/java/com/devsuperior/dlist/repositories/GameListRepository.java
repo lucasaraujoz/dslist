2f7a1acae6c431b1a6fb8f4bd73d0c616d8daa3c
Lucas Araujo <lcsaraujo88@gmail.com>
2023-07-16 15:36:40 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dlist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dlist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dlist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
